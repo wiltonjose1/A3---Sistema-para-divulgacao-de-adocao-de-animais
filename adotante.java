0b9e3c91e6bfdef74d9162dcc2e714f7bb547923
Diego Silva <112778845+DiegoSilvaCBA@users.noreply.github.com>
2022-11-03 10:29:18 -0300

    modificando class

--- adotante.java
+++ adotante.java
@@ -1,3 +1,4 @@
-public class animal {
+
+public class adotante {
 
 }
--- animal.java
+++ animal.java
@@ -1,3 +1,54 @@
 public class animal {
+    public int id_animal;
 
+    private String especie;
+
+    private String idade;
+
+    private String raça;
+
+    private String endereco;
+
+    private int telefone;
+
+    
+    public int getId_animal() {
+		return id_animal;
+	}
+
+	public void setId_animal(int id_animal) {
+		this.id_animal = id_animal;
+    }    
+
+    public String getNome() {
+		return especie;
+	}
+
+	public void setNome(String especie) {
+		this.especie = especie;
+	}
+
+    public String getIdade() {
+		return idade;
+	}
+
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+
+    public int getTelefone() {
+		return telefone;
+	}
+
+	public void setDescricao(int telefone) {
+		this.telefone = telefone;
+	}
+
+    public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEdereco(String endereco) {
+		this.endereco = endereco;
+	}
 }
