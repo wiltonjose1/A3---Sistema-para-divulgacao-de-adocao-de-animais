3bcbdb6d6eb0f0ee28c4985f06fb8217599bed23
Tiago Antunes <ddhd746@gmail.com>
2022-11-03 11:18:45 -0300

    Classe

--- Classes/Imagem.java
+++ Classes/Imagem.java
@@ -1,1 +1,20 @@
 
+private int id_imagem;
+
+    private String url;
+
+    public int getId_imagem() {
+        return id_imagem;
+    }
+
+    public void setId_imagem(int id_imagem) {
+        this.id_imagem = id_imagem;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
