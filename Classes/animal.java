f487f8860afbf985af873814bd6cb48724792654
Wilton José <116817558+wiltonjose1@users.noreply.github.com>
2022-11-11 10:02:19 -0300

    Update animal.java

--- Classes/animal.java
+++ Classes/animal.java
@@ -10,6 +10,19 @@ public class animal {
     private String endereco;
 
     private int telefone;
+	
+    private boolean estahComVacinasEmDia;
+
+    private boolean estahCastrado; 
+	
+    private String detalhes;
+
+    private String cor;
+	
+    private String sexo;
+ 
+    private String tamanho;
+
 
     
     public int getId_animal() {
@@ -59,4 +72,53 @@ public class animal {
 	public void setEdereco(String endereco) {
 		this.endereco = endereco;
 	}
+	
+	public boolean isEstahComVacinasEmDia() {
+		return estahComVacinasEmDia;
+	}
+
+	public void setEstahComVacinasEmDia(boolean estahComVacinasEmDia) {
+		this.estahComVacinasEmDia = estahComVacinasEmDia;
+	}
+	
+	public boolean isEstahCastrado() {
+		return estahCastrado;
+	}
+
+	public void setEstahCastrado(boolean estahCastrado) {
+		this.estahCastrado = estahCastrado;
+	}
+	
+	public void setDetalhes(String detalhes) {
+		this.detalhes = detalhes;
+	}
+	
+	public void setDetalhes(String detalhes) {
+		this.detalhes = detalhes;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public String getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+		
 }
